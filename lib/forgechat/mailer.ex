f31d3705251f06b22fcaf05f61f06736edc170b1
antonin <r.antonin.pro@gmail.com>
2026-09-11 23:54:18 +0200

    feat: add Phoenix skeleton (API-only, no Ecto)

---
+++ lib/forgechat/mailer.ex
@@ -0,0 +1,3 @@
+defmodule ForgeChat.Mailer do
+  use Swoosh.Mailer, otp_app: :forgechat
+end
--- test/forge_chat_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ForgeChatTest do
-  use ExUnit.Case
-  doctest ForgeChat
-
-  test "greets the world" do
-    assert ForgeChat.hello() == :world
-  end
-end
